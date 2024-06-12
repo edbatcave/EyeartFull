f1a50e02b52281ce8c73e933753534162bd6b898
edbatcave <161376570+edbatcave@users.noreply.github.com>
2024-06-12 17:27:08 -0500

    Update producto.js

--- EyeArt admins/EyeArt/Backend/producto.js
+++ EyeArt admins/EyeArt/Backend/producto.js
@@ -78,9 +78,9 @@ function RegisterProducto(app){
 // -- ACTUALIZAR PRODUCTO --
     app.put(`/${moduleName}/editar/:id`,(req,res)=>{
         const {id} = req.params;
-        const {Nombre, DescripcionProducto,Precio,Stock,url_foto,Categoria_idCategoria} = req.body;
+        const {Nombre, DescripcionProducto, Precio, Stock, url_foto, Categoria_idCategoria} = req.body;
         const query = `UPDATE ${moduleName} SET Nombre = '${Nombre}', DescripcionProducto = '${DescripcionProducto}' , Precio = '${Precio}',
-        Stock = '${Stock}' , Categoria_idCategoria = '${Categoria_idCategoria}' WHERE idProducto=${id}`;
+        Stock = '${Stock}', url_foto = '${url_foto}', Categoria_idCategoria = '${Categoria_idCategoria}' WHERE idProducto=${id}`;
         conexion.query(query, (error, resultado)=>{
             if(error) return console.error(error.message)   
     
